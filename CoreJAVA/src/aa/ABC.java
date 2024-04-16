56b11a9c16f4854831316edc4ae159f827e8092d
jay-java <jayprajapati.tops@gmail.com>
2024-04-16 18:15:50 +0530

    first commit

---
+++ CoreJAVA/src/aa/ABC.java
@@ -0,0 +1,10 @@
+package aa;
+
+public class ABC {
+		public void call() {
+			System.out.println("call function");
+		}
+		public void run() {
+			System.out.println("run function");
+		}
+	}
---
+++ CoreJAVA/src/basic/First.java
@@ -0,0 +1,17 @@
+package basic;
+//1.in
+//2.out
+//3.err
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("hello java");
+		System.out.println("hello again");
+		int i = 12;
+		int j =134;	
+		double d = 3.14;
+		System.out.println();
+		int k = i*j;
+		System.out.println("k = "+k);
+	}
+}
