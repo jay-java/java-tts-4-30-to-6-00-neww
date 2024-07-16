725718426cb2cacf0ccfd162818d57afc8248ac3
jay-java <jayprajapati.tops@gmail.com>
2024-07-16 17:35:42 +0530

    spring core first

---
+++ SpringCore/src/main/java/com/core/App.java
@@ -0,0 +1,12 @@
+package com.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext con = new ClassPathXmlApplicationContext("com/core/config.xml");
+		User u1 =(User)con.getBean(User.class);
+		System.out.println(u1);
+	}
+}
---
+++ SpringCore/src/main/java/com/core/User.java
@@ -0,0 +1,37 @@
+package com.core;
+
+public class User {
+	private int id;
+	private String name;
+	private long contact;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", contact=" + contact + ", address=" + address + "]";
+	}
+	
+}
